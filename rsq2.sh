ab73ecde686014f6c4fcad35457c0f4a9466d989
Vũ Thị Vân Anh <83341829+mollymendis@users.noreply.github.com>
2023-09-10 14:10:07 +0700

    Create rsq2.sh

---
+++ rsq2.sh
@@ -0,0 +1,24 @@
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+clear
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 5900 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+echo "Starting Windows"
+qemu-system-x86_64 -hda win2012.qcow2 -m 4G -cpu host -vnc :0 -smp cores=2 -net nic,bus=pci.0 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -enable-kvm -accel kvm &>/dev/null &
+qemu-system-x86_64 -hda win2012.qcow2 -m 4G -vnc :0 -smp cores=2  -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga qxl -accel kvm &>/dev/null &
+clear
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+sleep 43200
