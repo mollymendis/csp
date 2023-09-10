6fab677d46c4140d25241228044a98ee930962a7
Vũ Thị Vân Anh <83341829+mollymendis@users.noreply.github.com>
2023-09-10 14:20:16 +0700

    Update rsq2.sh

--- rsq2.sh
+++ rsq2.sh
@@ -15,7 +15,6 @@ read -p "choose ngrok region: " CRP
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 echo "Starting Windows"
-qemu-system-x86_64 -hda win2012.qcow2 -m 4G -cpu host -vnc :0 -smp cores=2 -net nic,bus=pci.0 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -enable-kvm -accel kvm &>/dev/null &
 qemu-system-x86_64 -hda win2012.qcow2 -m 4G -vnc :0 -smp cores=2  -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga qxl -accel kvm &>/dev/null &
 clear
 echo RDP Address:
