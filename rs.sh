4e8513c9f82ff9ad7c497ee1eea9def197ea8f3c
Vũ Thị Vân Anh <83341829+mollymendis@users.noreply.github.com>
2023-09-10 15:01:10 +0700

    Create rs.sh

---
+++ rs.sh
@@ -0,0 +1,24 @@
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+clear
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 5900 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+clear
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Starting Windows"
+sudo qemu-system-x86_64 -m 4G -cpu host -boot order=c -drive file=win2012.iso,media=cdrom -drive file=win2012.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=2 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
+echo "Note: Use Right-Click Or Ctrl+Shift+C To Copy"
+echo "Please Keep Tab Open"
+sleep 43200
