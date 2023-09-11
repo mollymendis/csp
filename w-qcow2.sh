83e134b13ab4feaec076e0467e7e40341c2f814a
Vũ Thị Vân Anh <83341829+mollymendis@users.noreply.github.com>
2023-09-11 23:46:35 +0700

    Update w-qcow2.sh

--- w-qcow2.sh
+++ w-qcow2.sh
@@ -8,4 +8,6 @@ echo "Download ngrok"
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xf ngrok.tgz > /dev/null 2>&1
 rm -rf ngrok.tgz
-wget -O rsq2.sh https://raw.githubusercontent.com/mollymendis/csp/main/rsq2.sh
+wget -O b1.sh https://raw.githubusercontent.com/mollymendis/csp/main/b1.sh
+wget -O b2.sh https://raw.githubusercontent.com/mollymendis/csp/main/b2.sh
+sudo bash b1.sh
